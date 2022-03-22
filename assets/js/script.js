87cb32f8af7c9d56c13e0d971456cc28a7503ff7
Quin Eddy <qkeddy@gmail.com>
2022-03-22 12:01:24 +0100

    Refactored the entire flow of the questions as I was previously at a dead end trying to build the question and answer elements inside of a function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,26 @@
+// Find elements on page
+var body = document.body;
+var h2El = document.querySelector("h2");
+var topicEl = document.querySelector("h3");
+var answerListEl = document.querySelector("#answer-list");
+
+var questionCount = 0;
+var currentQuestionNum = 0;
+
 /**
  * ! Populate initial HTML screen
  */
-function buildWelcomePage() {
-    // Initialize elements
-    var body = document.body;
-    var h2El = document.createElement("h2");
-    var infoEl = document.createElement("div");
+function buildWelcomePage(questionNumber) {
+    // Initialize temporary button
     var startQuizEl = document.createElement("button");
 
     // Add text to elements
-    h2El.textContent =
-        "Test your knowledge about the Powers of Matthew Star Television Series";
-    infoEl.textContent =
+    topicEl.textContent =
         "Try to answer the following questions within the 60 second time limit. Remember that incorrect answers will penalize your score time by ten seconds! Good luck!";
     startQuizEl.textContent = "Start Challenge!";
 
     // Add the elements to the body tag
-    body.append(h2El, infoEl, startQuizEl);
+    body.append(startQuizEl);
 
     console.log("Welcome page built");
 
@@ -25,12 +29,11 @@ function buildWelcomePage() {
         // Override default HTML form behavior
         event.preventDefault();
 
-        // Remove unnecessary tags
-        infoEl.remove();
+        // Remove start quiz button
         startQuizEl.remove();
 
         // Start Quiz
-        buildQuestionPage();
+        buildQuestionPage(questionNumber);
     });
 }
 
@@ -39,55 +42,30 @@ function buildWelcomePage() {
  */
 
 function buildQuestionPage(questionNumber) {
-    // Initialize elements
-    var body = document.body;
-    var questionEl = document.createElement("h2");
+    // console.log("Question #" + (questionNumber + 1).toString());
 
-    // Elements to the body tag
-    body.append(questionEl);
+    // Get current question
+    const currentQuestion = supplyQuestions(questionNumber);
 
-    // Fetch number of questions
-    var questionCount = supplyQuestions().quizQuestions.length;
+    // Populate the question on the <h3> tag
+    topicEl.textContent = currentQuestion.question();
 
-    // For each question, build the question page
-    for (let i = 0; i < questionCount; i++) {
-        // Get current question
-        const currentQuestion = supplyQuestions(i);
+    // Flush the possible answer ordered list
+    answerListEl.innerHTML = "";
 
-        // Clear multiple choice questions and corresponding event listener; prepare to populate
-        if (quizListEl) {
-            quizListEl.remove();
-        }
-
-        var quizListEl = document.createElement("ol");
-        body.append(quizListEl);
-
-        // Populate the question add to body tag
-        questionEl.textContent = currentQuestion.question();
-
-        // Populate the multiple choice answers
-        const possibleAnswers = currentQuestion.possibleAnswers();
-        for (let i = 0; i < possibleAnswers.length; i++) {
-            const multipleChoiceEl = document.createElement("li");
-            multipleChoiceEl.innerHTML =
-                '<a href="#">' + possibleAnswers[i] + "</a>";
-            quizListEl.appendChild(multipleChoiceEl);
-        }
-
-        // Add an event listener when a multiple choice answer is selected
-        quizListEl.addEventListener("click", function (event) {
-            var element = event.target;
-            if (element.matches("a") === true) {
-                alert("hello");
-            }
-        });
-
-        // Log questions details
-        console.log("Question: " + currentQuestion.question());
-        console.log("Choices: " + currentQuestion.possibleAnswers());
-        console.log("Answer: " + currentQuestion.correctAnswer());
-        console.log("Answer ID: " + currentQuestion.correctAnswerId());
+    // Populate the multiple choice answers
+    const possibleAnswers = currentQuestion.possibleAnswers();
+    for (let i = 0; i < possibleAnswers.length; i++) {
+        const answerEl = document.createElement("li");
+        answerEl.innerHTML = '<a href="#">' + possibleAnswers[i] + "</a>";
+        answerListEl.appendChild(answerEl);
     }
+
+    // Log questions details
+    // console.log("Question: " + currentQuestion.question());
+    // console.log("Choices: " + currentQuestion.possibleAnswers());
+    // console.log("Answer: " + currentQuestion.correctAnswer());
+    // console.log("Answer ID: " + currentQuestion.correctAnswerId());
 }
 
 /**
@@ -147,7 +125,40 @@ function supplyQuestions(questionNumber) {
  * ! Main Controller
  */
 function init() {
-    buildWelcomePage();
+    // Fetch number of questions
+    questionCount = supplyQuestions().quizQuestions.length;
+    console.log("There are " + questionCount + " question(s) in this quiz.");
+
+    // Build the welcome page if the question count is greater than 0
+    if (questionCount > 0) {
+        buildWelcomePage(0);
+    }
+
+    // Increment the question count since the first question has loaded
+    currentQuestionNum++;
 }
 
 init();
+
+// Add an event listener when a multiple choice answer is selected
+answerListEl.addEventListener("click", function (event) {
+    var element = event.target;
+
+    console.log("Current Question Number " + currentQuestionNum);
+    console.log("Question Count " + questionCount);
+
+
+    if (element.matches("a") === true) {
+        if (currentQuestionNum <= questionCount - 1) {
+            // Build the next question and set of answers
+            buildQuestionPage(currentQuestionNum);
+
+            // Increment question count
+            currentQuestionNum++;
+        }
+    }
+
+});
+
+// TODO add a remaining questions
+// TODO add the MS questions
